32820f879620b31c102b935fd49fa6819e706980
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2015-06-19 14:45:08 +0200

    update the files to get location updates

--- mobile/src/main/java/de/ludetis/android/myheartbeat/MainActivity.java
+++ mobile/src/main/java/de/ludetis/android/myheartbeat/MainActivity.java
@@ -1,32 +1,91 @@
 package de.ludetis.android.myheartbeat;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private TextView textView;
+    GoogleApiClient mGoogleApiClient;
+    Location mCurrentLocation;
 
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             // message from API client! message from wear! The contents is the heartbeat.
-            if(textView!=null)
-                textView.setText(Integer.toString(msg.what));
+            Toast.makeText(MainActivity.this, "handleMessage" + mCurrentLocation, Toast.LENGTH_SHORT).show();
+            if(textView!=null && mCurrentLocation!=null)
+                textView.setText(Integer.toString(msg.what) + mCurrentLocation.toString());
+            else if(textView !=null)
+                textView.setText(Integer.toString(msg.what) + "null");
         }
+
     };
+
+    protected synchronized void buildGoogleApiClient() {
+        Toast.makeText(this, "buildGoogleApiClient", Toast.LENGTH_SHORT).show();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
+
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        startLocationUpdates();
+        Toast.makeText(this, "location" + mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    protected void startLocationUpdates() {
+        Toast.makeText(this, "startLocationUpdates", Toast.LENGTH_SHORT).show();
+
+        LocationRequest mLocationRequest = createLocationRequest();
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    protected LocationRequest createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.heartbeat);
+        buildGoogleApiClient();
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -34,6 +93,9 @@ public class MainActivity extends Activity {
         super.onResume();
         // register our handler with the DataLayerService. This ensures we get messages whenever the service receives something.
         DataLayerListenerService.setHandler(handler);
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
     }
 
     @Override
@@ -41,6 +103,21 @@ public class MainActivity extends Activity {
         // unregister our handler so the service does not need to send its messages anywhere.
         DataLayerListenerService.setHandler(null);
         super.onPause();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Toast.makeText(this, "onConnectionFailed", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+//        Toast.makeText(this, "location changed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "location changed" + mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
+
     }
 }
 
