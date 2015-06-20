6fbbb97d2b787d27c9d62d3ec62a1957563dfabe
Abdelrahman Hussein <abdelrahman.hussein@hgst.com>
2015-06-20 14:50:26 +0200

    write csv file with heartbeat data and lat,lng and timestamp

--- mobile/src/main/java/de/ludetis/android/myheartbeat/MainActivity.java
+++ mobile/src/main/java/de/ludetis/android/myheartbeat/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.location.Location;
 import android.os.Handler;
 import android.os.Message;
-import android.provider.ContactsContract;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,19 +15,58 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import android.os.Environment;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+
 public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private TextView textView;
     GoogleApiClient mGoogleApiClient;
     Location mCurrentLocation;
+    FileWriter writer;
+    File root = Environment.getExternalStorageDirectory();
+    ArrayList<String> content = new ArrayList<String>();
+    String header = "lat,lng,time,heartbeat\n";
+
+    private void writeCsvData(ArrayList<String> data) throws IOException {
+        File gpxfile = new File(root, String.format("labs_heartrate_%s.csv", new Timestamp(new java.util.Date().getTime()).getTime()));
+        writer = new FileWriter(gpxfile);
+        writer.write(header);
+        for (String item : data){
+            writer.write(item);
+        }
+        writer.flush();
+        writer.close();
+
+    }
 
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             // message from API client! message from wear! The contents is the heartbeat.
-            Toast.makeText(MainActivity.this, "handleMessage" + mCurrentLocation, Toast.LENGTH_SHORT).show();
-            if(textView!=null && mCurrentLocation!=null)
-                textView.setText(Integer.toString(msg.what) + mCurrentLocation.toString());
+//            Toast.makeText(MainActivity.this, "handleMessage" + mCurrentLocation, Toast.LENGTH_SHORT).show();
+            if(textView!=null && mCurrentLocation!=null) {
+                textView.setText(Integer.toString(msg.what));
+                String value = String.format("%f,%f,%s,%d\n", mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(),
+                        new Timestamp(new java.util.Date().getTime()).getTime(), msg.what);
+                if (content.size() == 500){
+                    try{
+                        writeCsvData(content);
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+
+                    content.clear();
+                }
+                content.add(value);
+//                Toast.makeText(MainActivity.this, "handleMessage: " + value, Toast.LENGTH_SHORT).show();
+            }
             else if(textView !=null)
                 textView.setText(Integer.toString(msg.what) + "null");
         }
@@ -47,18 +84,16 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
-
+//        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
         mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         startLocationUpdates();
-        Toast.makeText(this, "location" + mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "location" + mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_SHORT).show();
-
+//        Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -115,9 +150,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     @Override
     public void onLocationChanged(Location location) {
         mCurrentLocation = location;
-//        Toast.makeText(this, "location changed", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "location changed" + mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
-
+//        Toast.makeText(this, "location changed" + mCurrentLocation.toString(), Toast.LENGTH_SHORT).show();
     }
 }
 
